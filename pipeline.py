f1438273515f68e5c52cc0124ee6c939dc2b12c4
Étienne Morice <etienne.morice@mpibpc.mpg.de>
2022-08-19 19:22:18 +0200

    feat(data): download metadata, filter tissues

--- pipeline.py
+++ pipeline.py
@@ -8,6 +8,7 @@ from contextlib import ExitStack
 
 import pyarrow as pa
 import pyarrow.csv as pacsv
+import pyarrow.compute as pc
 
 import galp
 
@@ -18,7 +19,7 @@ URLS = {
     ## Url of public RNA-seq counts
     'counts': 'https://storage.googleapis.com/gtex_analysis_v8/rna_seq_data/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct.gz',
     ## Url of corresponding meta data, mostly tissue type
-    'meta': 'https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt'
+    'sample_info': 'https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt'
 }
 
 ## Buffer size for streaming raw RNA-seq
@@ -73,6 +74,48 @@ def gex_counts_table(_galp):
 
     return path
 
+@step
+def gex_sample_info_table():
+    """
+    Download the sample information to an arrow table.
+
+    It's small so we directly check it in the store.
+    """
+    with urllib.request.urlopen(URLS['sample_info']) as response:
+        return pacsv.read_csv(response,
+                parse_options=pacsv.ParseOptions(delimiter='\t')
+                )
+
 downloads = [
-    gex_counts_table
+    gex_counts_table,
+    gex_sample_info_table,
     ]
+
+@step(vtag=1)
+def gex_tissue_counts_table(tissue_name, gex_counts_table, gex_sample_info_table):
+    """
+    Filter a single tissue from the counts table, and checks the result in store.
+    """
+    sampids = (gex_sample_info_table
+            .filter(pc.field('SMTSD') == tissue_name)
+            ['SAMPID']
+            .to_pylist()
+            )
+
+    out_batches = []
+    with pa.ipc.open_file(gex_counts_table) as reader:
+        names = []
+        for i in range(reader.num_record_batches):
+            in_batch = reader.get_batch(i)
+            if not i:
+                names = [ name
+                    for name in ['Name', 'Description'] + sampids
+                    if name in in_batch.schema.names
+                    ]
+            out_batches.append(
+                pa.RecordBatch.from_arrays([
+                    in_batch[name]
+                    for name in names
+                    ], names=names)
+                )
+        return pa.Table.from_batches(out_batches)
